0cb8b16f7b6f862580ca112a1f7b7d6bf5410a9f
EbroDore <brodura1322@hotmail.com>
2024-06-18 22:17:25 +1000

    Add project files.

---
+++ RestAssured/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RestAssured.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
