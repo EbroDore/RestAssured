e65851db1612fbb81299276fcb9851ec4c0229d1
EbroDore <brodura1322@hotmail.com>
2024-06-18 22:26:47 +1000

    Added Sql Database, Created Products model and tables

--- RestAssured/Data/ApplicationDbContext.cs
+++ RestAssured/Data/ApplicationDbContext.cs
@@ -1,13 +1,26 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using RestAssured.Models;
 
 namespace RestAssured.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2)");
+
+            base.OnModelCreating(builder);
         }
     }
 }
